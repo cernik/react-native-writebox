43c0d371d5c16cf56dda4703a5b5a7c039bec1f7
cernik <milas.etc@gmail.com>
2017-07-18 18:11:06 +0300

    remove proptypes prettier

--- src/WriteboxInput.js
+++ src/WriteboxInput.js
@@ -1,32 +1,14 @@
 /* @flow */
-import React, { Component, PropTypes } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Platform
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, Platform } from 'react-native';
 
-import {
-  LIGHT_GRAY_FONT_COLOR,
-  GRAY_FONT_COLOR
-} from './constants'
+import { LIGHT_GRAY_FONT_COLOR, GRAY_FONT_COLOR } from './constants';
 
-import styles from './styles'
+import styles from './styles';
 
-const INPUT_MAX_HEIGHT = 60
+const INPUT_MAX_HEIGHT = 60;
 
 export default class WriteBoxInput extends Component {
-  static propTypes = {
-    onHeightChanged: PropTypes.func,
-    onChange: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    autoFocus: PropTypes.bool,
-    value: PropTypes.string,
-    placeholder: PropTypes.string,
-    inputLimit: PropTypes.number
-  }
   static defaultProps = {
     onHeightChanged: () => {},
     onChange: () => {},
@@ -35,16 +17,16 @@ export default class WriteBoxInput extends Component {
     autoFocus: false,
     value: '',
     placeholder: 'Type here ...',
-    inputLimit: undefined
-  }
+    inputLimit: undefined,
+  };
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       content: props.value || '',
       height: 0,
-    }
-    this._onContentSizeChange = this._onContentSizeChange.bind(this)
-    this._onChange = this._onChange.bind(this)
+    };
+    this._onContentSizeChange = this._onContentSizeChange.bind(this);
+    this._onChange = this._onChange.bind(this);
   }
   componentWillReceiveProps(props) {
     if (props.autoFocus) {
@@ -53,78 +35,72 @@ export default class WriteBoxInput extends Component {
     this.setState({ content: props.value });
   }
   _maxInputHeight(input) {
-    return input >= INPUT_MAX_HEIGHT ? INPUT_MAX_HEIGHT : input
+    return input >= INPUT_MAX_HEIGHT ? INPUT_MAX_HEIGHT : input;
   }
   _onContentSizeChange(event) {
     let update = {
-      height: this._maxInputHeight(event.nativeEvent.contentSize.height)
-    }
-    this.setState(update)
-    this.props.onHeightChanged(update)
+      height: this._maxInputHeight(event.nativeEvent.contentSize.height),
+    };
+    this.setState(update);
+    this.props.onHeightChanged(update);
   }
   _onChange(event) {
     this.setState({
-      content:  event.nativeEvent.text
-    })
+      content: event.nativeEvent.text,
+    });
     if (Platform.OS === 'android') {
-      this._onContentSizeChange(event)
+      this._onContentSizeChange(event);
     }
     if (this.props.onChange) {
       this.props.onChange({ value: event.nativeEvent.text });
     }
   }
   render() {
-    let inputCounter
-    let addFlex, addFlexSize
+    let inputCounter;
+    let addFlex, addFlexSize;
     /*
      * Add counter
      */
     if (this.props.inputLimit) {
-      addFlex = {flexDirection: 'row', alignItems: 'flex-end' }
-      addFlexSize = { flex: .9 }
-      let remainder = this.props.inputLimit - this.state.content.length
+      addFlex = { flexDirection: 'row', alignItems: 'flex-end' };
+      addFlexSize = { flex: 0.9 };
+      let remainder = this.props.inputLimit - this.state.content.length;
       if (remainder < -99) {
-        remainder = -99
+        remainder = -99;
       }
-      const remainderColor = remainder > 5 ? GRAY_FONT_COLOR : 'red'
+      const remainderColor = remainder > 5 ? GRAY_FONT_COLOR : 'red';
       inputCounter = (
-        <Text style={
-            [ styles.remainder, { color: remainderColor } ]
-          }>
-          { remainder }
+        <Text style={[styles.remainder, { color: remainderColor }]}>
+          {remainder}
         </Text>
-      )
+      );
     }
     return (
-      <View style={[ styles.content, addFlex ] }>
+      <View style={[styles.content, addFlex]}>
         <TextInput
-          placeholder={ this.props.placeholder }
-          placeholderTextColor={ LIGHT_GRAY_FONT_COLOR }
-          autoFocus={ this.props.autoFocus }
-          onFocus={ this.props.onFocus }
-          onBlur={ this.props.onBlur }
-          accessible={ true }
-          accessibilityLabel={ this.props.placeholder }
-          multiline={ true }
+          placeholder={this.props.placeholder}
+          placeholderTextColor={LIGHT_GRAY_FONT_COLOR}
+          autoFocus={this.props.autoFocus}
+          onFocus={this.props.onFocus}
+          onBlur={this.props.onBlur}
+          accessible={true}
+          accessibilityLabel={this.props.placeholder}
+          multiline={true}
           returnKeyType="done"
-          ref={(r) => { this._textInput = r; }}
-          onContentSizeChange={ this._onContentSizeChange }
-          disableFullscreenUI={ true }
-          textBreakStrategy={ 'highQuality' }
-          onChange={ this._onChange.bind(this) }
-          underlineColorAndroid='transparent'
+          ref={r => {
+            this._textInput = r;
+          }}
+          onContentSizeChange={this._onContentSizeChange}
+          disableFullscreenUI={true}
+          textBreakStrategy={'highQuality'}
+          onChange={this._onChange.bind(this)}
+          underlineColorAndroid="transparent"
           enablesReturnKeyAutomatically={true}
-          style={
-            [
-              styles.input,
-              { height: this.state.height + 4},
-              addFlexSize
-            ]
-          }
-          value={ this.state.content }
-          />
-        { inputCounter }
+          style={[styles.input, { height: this.state.height + 4 }, addFlexSize]}
+          value={this.state.content}
+        />
+        {inputCounter}
       </View>
-    )
+    );
   }
 }
